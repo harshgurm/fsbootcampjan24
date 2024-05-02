0bb360f6d47ffc7cf2797f1cf2111cfaf84ae9fc
Harsh <harshgurm@gmail.com>
2024-05-01 21:04:04 -0400

    updated the connection string to connect to render.com

--- express_mariadb_tutorial/connection.js
+++ express_mariadb_tutorial/connection.js
@@ -1,11 +1,19 @@
 const mariadb = require('mariadb');
 
+// const pool = mariadb.createPool({
+//     host: 'localhost', 
+//     user: 'root',
+//     password: 'root',
+//     database: 'robogarden',
+//     port: 3360
+// });
+
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'robogarden',
-    port: 3360
+    host: 'dpg-copbmo8l6cac73ajqm50-a', 
+    user: 'database_example_5tf7_user',
+    password: 'zVZlAS7cqmZW2a74kuFW7G37QxZKpThf',
+    database: 'database_example_5tf7',
+    port: 5432
 });
 
 module.exports = pool;
